e5331fbc22ed6cc2fec985f3b3bfd500a3c4b425
Calisto <46696171+Calistu@users.noreply.github.com>
2019-08-17 03:52:20 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,9 +1,22 @@
-echo "\e[31mchecking the dependences\e[0m...\n"
-apt install libc6 
-apt install libgtk3-nocsd0 
+echo "\e[31m\nchecking the dependences...\e[0m\n"
+if [ -e /usr/lib/gcc ] ; then
+echo "\e[32mgcc checked!\e[0m\n"
+else
 apt install gcc
+fi
+if [ -e /bin/gzip ] ; then
+echo "\e[32mgzip checked!\e[0m\n"
+else
 apt install gzip
-echo "\n\e[93minstalling\e[0m...\n"
+fi
+if [ -e /usr/share/wordlisto ] ;
+then 
+echo "\e[93mUpdating...\e[0m\n"
+else
+mkdir /usr/share/wordlisto
+fi
+
+echo "\e[93minstalling...\e[0m\n"
 gcc wordlisto.c -o /bin/wordlisto
 chmod +x /bin/wordlisto
 cp -f /bin/wordlisto /usr/local/bin/
@@ -11,7 +24,6 @@ chmod +x /usr/local/bin/wordlisto
 cp -f man/wordlisto.1 /usr/share/man/man1/
 gzip -q -f /usr/share/man/man1/wordlisto.1
 chmod +x /usr/share/man/man1/wordlisto.1.gz
-mkdir /usr/share/wordlisto
 cp -f -r DB /usr/share/wordlisto
 chmod +x /usr/share/wordlisto
 if [ -e /bin/wordlisto ] ; then
