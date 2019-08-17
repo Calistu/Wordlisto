8a85abb93a8c110382cc63d612d84c10ac6d5c52
Calisto <46696171+Calistu@users.noreply.github.com>
2019-08-17 12:51:45 -0300

    update
    now ask for sudo by oneself
    to prevent bad installation

--- install.sh
+++ install.sh
@@ -2,32 +2,34 @@ echo "\e[31m\nchecking the dependences...\e[0m\n"
 if [ -e /usr/lib/gcc ] ; then
 echo "\e[32mgcc checked!\e[0m\n"
 else
-apt install gcc
+sudo apt-get install gcc
 fi
 if [ -e /bin/gzip ] ; then
 echo "\e[32mgzip checked!\e[0m\n"
 else
-apt install gzip
+sudo apt-get install gzip
 fi
 if [ -e /usr/share/wordlisto ] ;
 then 
 echo "\e[93mUpdating...\e[0m\n"
 else
-mkdir /usr/share/wordlisto
+sudo mkdir /usr/share/wordlisto
+fi
+echo "\e[93minstalling...\e[0m\n"
+sudo gcc wordlisto.c -o /bin/wordlisto
+sudo chmod +x /bin/wordlisto
+sudo cp -f /bin/wordlisto /usr/local/bin/
+sudo chmod +x /usr/local/bin/wordlisto
+sudo cp -f man/wordlisto.1 /usr/share/man/man1/
+sudo gzip -q -f /usr/share/man/man1/wordlisto.1
+sudo chmod +x /usr/share/man/man1/wordlisto.1.gz
+sudo cp -f -r DB /usr/share/wordlisto
+sudo chmod +x /usr/share/wordlisto
+if [ -e /bin/wordlisto ] ; then
+echo "\e[32minstaled successfuly...\e[0m\n"
+echo "\e[32mUse wordlisto --help\e[0m\n"
+else
+echo "\e[31mERROR IN INSTALATION\n\e[0m"
+echo "\e[31mDid you use the 'sudo'?\n\e[0m"
 fi
 
-echo "\e[93minstalling...\e[0m\n"
-gcc wordlisto.c -o /bin/wordlisto
-chmod +x /bin/wordlisto
-cp -f /bin/wordlisto /usr/local/bin/
-chmod +x /usr/local/bin/wordlisto
-cp -f man/wordlisto.1 /usr/share/man/man1/
-gzip -q -f /usr/share/man/man1/wordlisto.1
-chmod +x /usr/share/man/man1/wordlisto.1.gz
-cp -f -r DB /usr/share/wordlisto
-chmod +x /usr/share/wordlisto
-if [ -e /bin/wordlisto ] ; then
-echo "\e[32minstaled\e[0m...\n"
-else
-echo "\e[31mERROR IN INSTALATION\e[0m"
-fi
--- unistall.sh
+++ unistall.sh
@@ -1,6 +1,12 @@
-echo "\e[31mRemoving...\e[0m"
-rm /bin/wordlisto
-rm /usr/local/bin/wordlisto
-rm -r -f /usr/share/wordlisto
-rm /usr/share/man/man1/wordlisto.1.gz
-echo "\e[92mRemoved.\e[0m"
+echo "\e[31mRemoving...\e[0m\n"
+sudo rm -f /bin/wordlisto 
+sudo rm -f /usr/local/bin/wordlisto
+sudo rm -r -f /usr/share/wordlisto
+sudo rm -f /usr/share/man/man1/wordlisto.1.gz
+if [ -e /bin/wordlisto ] ;
+then
+echo "\e[31mERROR\e[0m\n"
+else
+echo "\e[92mRemoved.\e[0m\n"
+fi
+exit
